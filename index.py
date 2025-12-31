b217629c2528e70d18b71264895ac388eff2f3fc
karthick-gmk <rkarthick9688@gmail.com>
2025-12-31 19:05:16 +0530

    vercel

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
 from myapp.wsgi import application
 
-# Vercel serverless function
-def handler(request):
-    return application(request.environ, request.start_response)
+# Vercel handler
+app = application
