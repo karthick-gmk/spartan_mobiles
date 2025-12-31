42a0efeb25781ad9fa7053f03fab0d7a7137c5c8
karthick-gmk <rkarthick9688@gmail.com>
2025-12-31 21:21:05 +0530

    vercel

---
+++ build_files.sh
@@ -0,0 +1,2 @@
+pip install -r requriements.txt
+python3.9 manage.py collectstatic 
--- myapp/settings.py
+++ myapp/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ln&%59o!%n34x$$hwvxdm71!&%r@8wsnsajgm)w)_%g@z8hjm('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
-ALLOWED_HOSTS = ['127.0.0.1',"*"]
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1',"*"]
 
 
 
