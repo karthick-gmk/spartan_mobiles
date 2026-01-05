6b2166d83b4b51f4cc7cf50661a3d221d4f4a9a5
karthick-gmk <rkarthick9688@gmail.com>
2026-01-05 19:57:15 +0530

    ss

--- myapp/settings.py
+++ myapp/settings.py
@@ -82,23 +82,23 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 AUTH_USER_MODEL = 'usermanagement.User'
 
-DATABASES = {
-    'default':{
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'spartan_mobile_db',
-        'USER': 'postgres',
-        'PASSWORD':'admin',
-        'HOST' : 'localhost',
-        'PORT': '5432'
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3', # 'NAME' is the path to your database file
+#     'default':{
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'spartan_mobile_db',
+#         'USER': 'postgres',
+#         'PASSWORD':'admin',
+#         'HOST' : 'localhost',
+#         'PORT': '5432'
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3', # 'NAME' is the path to your database file
+    }
+}
 
 
 # Password validation
